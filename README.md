5b981f7c446d33805317e13b07522c7b7aeb3e85
Antonio Moya <ajmoyad@gmail.com>
2014-03-13 14:10:11 +0100

    subida de ficheros

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-image-registration
-==================
+### Image Registration
+#### Coloreando imágenes del Imperio Ruso
+
+Scripts realizado en el marco de la asignatura ‘Procesado Digital de Imágenes’, Ingeniería de Telecomunicaciones, 4º Curso, Universidad de Granada.
+
+***
+
+Se trata de una posible solución al problema planteado en (http://graphics.cs.cmu.edu/courses/15-463/2008_fall/hw/proj1/ "Programming Project #1 (proj1)")
+
+Para la realización del mismo se ha usado un algoritmo piramidal para posibilitar la tarea en imágenes de muy alta resolución. El rectificado se realiza aplicando correlaciones bidimensionales. Además se añaden labores de mejora del contraste y de recortado automático de los bordes.
+
+En todo momento se persigue la eficiencia del algoritmo, para lo cuál se incluyen también, en el fichero de demostración, un cálculo del tiempo de proceso necesitado en las distintas tareas.
+
+***
+
+Las imágenes de muy alta resolución, debido a su tamaño, no se incluyen en este ejemplo. Se pueden encontrar siguiendo el enlace de arriba.
+
+***
+
+Autor: Antonio J. Moya Díaz - ajmoyad@gmail.com
+Fecha: 5 de Junio de 2013
---
+++ demo.m
@@ -0,0 +1,240 @@
+% PROYECTO: COLOREANDO IMÁGENES DEL IMPERIO RUSO
+%
+%   Script de demostración y generación de estadísticas del proyecto de
+%   registrado de imágenes de la asignatura Laboratorio Multimedia.
+%
+%   Antonio J. Moya Díaz
+%   5 de Junio de 2013
+
+
+%% INFO
+% Este fichero está organizado en 3 celdas de ejecución. 
+%   - La primera de ellas carga los nombres de las imágenes en una variable.
+% Es importante para la correcta ejecución que el directorio sea el que se
+% indica.
+%   - La segunda celda es para la ejecución de 1 única imagen y así poder
+% observar su resultado. Debe seleccionar la imagen a procesar.
+%   - La tercera celda automatiza una batería de operaciones con el fin de
+% evaluar los tiempos de ejecución. Se ha pensado para tratar las imágenes
+% pequeñas y las imágenes por separado. Se puede activar o desactivar la
+% ecualización, así como seleccionar el número de iteraciones. AVISO, con
+% imagenes grandes y muchas iteraciones, la ejecución tardará varios
+% minutos.
+
+%% Preliminar
+clear, close all, clc
+
+% Lista de ficheros de imagen, para sencillez en la carga posterior
+f=cell(1,16);
+
+f{1}='images/00125v.jpg';
+f{2}='images/00149v.jpg';
+f{3}='images/00153v.jpg';
+f{4}='images/00154v.jpg';
+f{5}='images/00163v.jpg';
+f{6}='images/00270v.jpg';
+f{7}='images/00398v.jpg';
+f{8}='images/00564v.jpg';
+f{9}='images/01167v.jpg';
+f{10}='images/31421v.jpg';
+%f{11}='images/00458u.tif';
+%f{12}='images/00911u.tif';
+%f{13}='images/01043u.tif';
+%f{14}='images/01047u.tif';
+%f{15}='images/01657u.tif';
+%f{16}='images/01861a.tif';
+
+
+%% Ejecución singular
+
+% SELECCIONE una imagen de entre la lista anterior
+imagen=16;
+% ------------------------------------------------
+
+
+
+% Selección automática de la ventana
+if imagen<11
+    vent_ini=8;
+else
+    vent_ini=20;
+end
+
+
+I=imread(f{imagen});
+I=im2double(I);
+    
+% Seccionado de la imagen en sus tres canales
+h=floor(size(I,1)/3);
+B=I(1:h,:);
+G=I(h+1:h*2,:);
+R=I(h*2+1:h*3,:);
+
+clear I h
+
+% Cálculo de los desplazamientos
+desp_Rojo=registraImagen(G,R,vent_ini);
+desp_Azul=registraImagen(G,B,vent_ini);
+
+% Reserva de espacio
+Ifinal=ones(size(R,1),size(R,2),3);
+Rr=zeros(size(R,1),size(R,2));
+Br=zeros(size(B,1),size(B,2));
+
+% Montado del canal Rojo
+dy=desp_Rojo(1);
+dx=desp_Rojo(2);
+Rr(max(1,1+dy):min(end,end+dy),max(1,1+dx):min(end,end+dx))=R(max(1,1-dy):min(end,end-dy),max(1,1-dx):min(end,end-dx));
+
+% Montado del canal azul
+dy=desp_Azul(1);
+dx=desp_Azul(2);
+Br(max(1,1+dy):min(end,end+dy),max(1,1+dx):min(end,end+dx))=B(max(1,1-dy):min(end,end-dy),max(1,1-dx):min(end,end-dx));
+    
+% Montado de la imagen final
+Ifinal(:,:,1)=Rr;
+Ifinal(:,:,2)=G;
+Ifinal(:,:,3)=Br;
+
+% Recorte de los bordes
+[corteY,corteX]=recortarBordes(Ifinal);
+Irecortada=Ifinal(corteY(1):end-corteY(2),corteX(1):end-corteX(2),:);
+
+% Ecualización
+Irecortada=imadjust(Irecortada,stretchlim(Irecortada),[]);
+
+% Muestra la imagen
+figure(31),imshow(Irecortada)
+
+
+
+
+
+%% Estadísticas de tiempos por grupo de imágenes
+
+% SELECCIONE los siguientes parámetros
+seleccion=1; % 0=>imágenes pequeñas, 1=>imágenes grandes
+ecualizacion=1; % 0=> desactivada, 1=> activada
+iteraciones=20; % Número de iteraciones
+% -------------------------------------
+
+
+
+
+if seleccion==0
+    n=10;
+    vent_ini=8;
+    offset=1;
+elseif seleccion==1
+    n=6;
+    vent_ini=20;
+    offset=11;
+else
+    error('No ha seleccionado un grupo de imagen correcto')
+end
+
+
+  
+tiempo_rojo=zeros(iteraciones,n);
+tiempo_azul=zeros(iteraciones,n);
+tiempo_corte=zeros(iteraciones,n);
+tiempo_ecu=zeros(iteraciones,n);
+tiempo_total=zeros(iteraciones,n);
+
+for j=1:1:iteraciones
+
+    for i=offset:1:offset+n-1;
+    
+        total=tic;
+    
+        % Cargamos la imagen
+        I=imread(f{i});
+        I=im2double(I);
+    
+        % Seccionado de la imagen en sus tres canales
+        h=floor(size(I,1)/3);
+        B=I(1:h,:);
+        G=I(h+1:h*2,:);
+        R=I(h*2+1:h*3,:);
+
+        clear I h
+    
+        % Registro canal rojo
+        red=tic;
+            desp_Rojo=registraImagen(G,R,vent_ini);
+        tiempo_rojo(j,i-offset+1)=toc(red);
+    
+        % Registro canal azul
+        blue=tic;
+            desp_Azul=registraImagen(G,B,vent_ini);
+        tiempo_azul(j,i-offset+1)=toc(blue);
+    
+        % Montado de la imagen final
+        Ifinal=ones(size(R,1),size(R,2),3);
+        Rr=zeros(size(R,1),size(R,2));
+        Br=zeros(size(B,1),size(B,2));
+    
+        dy=desp_Rojo(1);
+        dx=desp_Rojo(2);
+        Rr(max(1,1+dy):min(end,end+dy),max(1,1+dx):min(end,end+dx))=R(max(1,1-dy):min(end,end-dy),max(1,1-dx):min(end,end-dx));
+
+        dy=desp_Azul(1);
+        dx=desp_Azul(2);
+        Br(max(1,1+dy):min(end,end+dy),max(1,1+dx):min(end,end+dx))=B(max(1,1-dy):min(end,end-dy),max(1,1-dx):min(end,end-dx));
+    
+        Ifinal(:,:,1)=Rr;
+        Ifinal(:,:,2)=G;
+        Ifinal(:,:,3)=Br;
+    
+        % Recorte de los bordes
+        cut=tic;
+            [corteY,corteX]=recortarBordes(Ifinal);
+        tiempo_corte(j,i-offset+1)=toc(cut);
+    
+        Irecortada=Ifinal(corteY(1):end-corteY(2),corteX(1):end-corteX(2),:);
+    
+        clear Ifinal
+    
+    % Ecualización
+    if ecualizacion==1
+        ecu=tic;
+            Irecortada=imadjust(Irecortada,stretchlim(Irecortada),[]);
+        tiempo_ecu(j,i-offset+1)=toc(ecu);
+    end
+    
+    
+        tiempo_total(j,i-offset+1)=toc(total);
+    
+        %disp(['Procesada la imagen ' f{i}]);
+    end
+
+    disp(['Completada iteración ' num2str(j)])
+
+end
+
+% Muestra de resultados
+if seleccion==0
+    texto='pequeñas';
+elseif seleccion==1
+    texto='grandes';
+end
+
+trojo_medio=mean(mean(tiempo_rojo));
+tazul_medio=mean(mean(tiempo_azul));
+tcut_medio=mean(mean(tiempo_corte));
+tecu_medio=mean(mean(tiempo_ecu));
+ttotal_medio=mean(mean(tiempo_total));
+
+disp(' ')
+disp(['Se han procesado las imágenes ' texto])
+disp('Tiempos medios:')
+disp(['Alineado canal rojo: ' num2str(trojo_medio)])
+disp(['Alineado canal azul: ' num2str(tazul_medio)])
+disp(['Cortado de bordes  : ' num2str(tcut_medio)])
+disp(['Ecualización       : ' num2str(tecu_medio)])
+disp(['Tiempo total       : ' num2str(ttotal_medio)])
+
+
+
+
+
